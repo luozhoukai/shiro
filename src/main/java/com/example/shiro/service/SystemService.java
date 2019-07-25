c4ca5dd81033fdeb1a4ec531d2d25ced4786b1ba
luozhoukai <luozhoukai@126.com>
2019-07-25 21:39:46 +0800

    删除多余代码

--- src/main/java/com/example/shiro/service/SystemService.java
+++ src/main/java/com/example/shiro/service/SystemService.java
@@ -3,23 +3,14 @@ package com.example.shiro.service;
 import com.example.shiro.config.ShiroRealm;
 import com.example.shiro.config.ShiroRedisCacheManager;
 import org.apache.shiro.cache.Cache;
-import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.filter.mgt.DefaultFilterChainManager;
-import org.apache.shiro.web.filter.mgt.PathMatchingFilterChainResolver;
-import org.apache.shiro.web.servlet.AbstractShiroFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class SystemService {
-    @Autowired
-    private ShiroRedisCacheManager shiroRedisCacheManager;
 
     @Autowired
-    private ShiroFilterFactoryBean shiroFilterFactoryBean;
+    private ShiroRedisCacheManager shiroRedisCacheManager;
 
     /**
      * 修改权限
